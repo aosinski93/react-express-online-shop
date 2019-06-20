3f43f8c1b4725a397481ad6c2385c11db61b8d8a
Adam <aosinski19@gmail.com>
2019-06-20 10:47:32 +0200

    add local connection to mongoDB as an alternative

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const router = require("./routes/index");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const router = require('./routes/index');
 
 const { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
-const db_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
+const db_URI =
+  DB_USER === undefined || DB_HOST === undefined || DB_NAME === undefined
+    ? 'mongodb://localhost:27017/react-express-online-shop'
+    : `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 mongoose.connect(db_URI, {
   useNewUrlParser: true
 });
 
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on('error', console.error.bind(console, 'connection error:'));
 
 app.use(cors());
 
@@ -25,10 +28,10 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.set("json spaces", 2);
+app.set('json spaces', 2);
 
 const port = process.env.PORT || 3001;
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
