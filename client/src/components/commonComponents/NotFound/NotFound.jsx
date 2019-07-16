e9d075b38bc9140ff5437dc951efd2137e35fbb3
Adam Osiński <aosinski93@gmail.com>
2019-07-16 14:03:49 +0200

    add custom 'Page not found' page

--- client/src/components/commonComponents/NotFound/NotFound.jsx
+++ client/src/components/commonComponents/NotFound/NotFound.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Container, Box } from "@material-ui/core";
+import { Link } from 'react-router-dom';
+import { Container, Box, Grid, Typography, Button } from "@material-ui/core";
 
 const NotFound = () => {
   return (
     <Container>
       <Box m={10}>
-        <h1>Page not found</h1>
+        <Grid container direction='column' justify='center' alignItems='center'>
+          <Grid item>
+            <Typography variant='h1'>
+              404
+          </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='h3'>
+              Page not found
+          </Typography>
+          </Grid>
+          <Grid item>
+            <Box m={5}>
+              <Typography>
+                The page you are looking for might have been removed or is temporarilly unavailable
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Button color="primary">
+              <Link to={'/'}>Go back to homepage</Link>
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
