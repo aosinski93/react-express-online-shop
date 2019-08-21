29bc26f2564a9353d907cff94fa06a9bc8506fa6
Adam <aosinski19@gmail.com>
2019-08-21 22:44:57 +0200

    add store persistor

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 import storeFunc from "./store";
 import AppContainer from "./containers/App.container";
+import Loader from "./components/commonComponents/Loader/Loader";
 
-const { store /* persistor */ } = storeFunc();
+const {store, persistor} = storeFunc();
 
 ReactDOM.render(
   <Provider store={store}>
-    <CssBaseline />
-    <AppContainer />
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <CssBaseline />
+      <AppContainer />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
